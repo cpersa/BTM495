5e9654ed1f8d00f2250be26f669866fbc7c930f6
charleain <183850672+charleain@users.noreply.github.com>
2024-10-29 23:11:10 -0400

    Use echo only in debug

--- src/renova/db.py
+++ src/renova/db.py
@@ -5,7 +5,7 @@ from sqlmodel import SQLModel, create_engine
 import renova.models  # noqa: F401
 
 DB_URL = os.getenv("DB_URL", "sqlite:////var/lib/renova/renova.db")
-ENGINE = create_engine(DB_URL, echo=True)
+ENGINE = create_engine(DB_URL, echo=os.getenv("DEBUG", False))
 
 
 def init_db():
