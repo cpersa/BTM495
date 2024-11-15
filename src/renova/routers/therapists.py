2c2e557d80076c52bf07c990b048bc898c621ea0
charleain <183850672+charleain@users.noreply.github.com>
2024-11-15 18:41:00 -0500

    added schedule endpoints

--- src/renova/routers/therapists.py
+++ src/renova/routers/therapists.py
@@ -2,17 +2,19 @@ from typing import Annotated, Sequence
 
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import AfterValidator
-from sqlmodel import Session, select
 from sqlalchemy.exc import IntegrityError
+from sqlmodel import Session, select
 
 from renova.db import get_session
-from renova.models.users import Therapist
+from renova.models import Appointment, Schedule, Therapist
+
 router = APIRouter()
 
 
 @router.post("/therapists")
 def post_therapists(
-    therapist: Annotated[Therapist, AfterValidator(Therapist.model_validate)], session: Session = Depends(get_session)
+    therapist: Annotated[Therapist, AfterValidator(Therapist.model_validate)],
+    session: Session = Depends(get_session),
 ) -> Therapist:
     t = Therapist.model_validate(therapist)
     try:
@@ -28,3 +30,45 @@ def post_therapists(
 @router.get("/therapists")
 def get_therapists(session: Session = Depends(get_session)) -> Sequence[Therapist]:
     return session.exec(select(Therapist)).all()
+
+
+@router.get("/therapist/{therapist_id}")
+def get_therapist(
+    therapist_id: int, session: Annotated[Session, Depends(get_session)]
+) -> Therapist:
+    return session.exec(select(Therapist).where(Therapist.id == therapist_id)).one()
+
+
+@router.post("/therapist/{therapist_id}/schedules")
+def post_therapist_schedules(
+    therapist_id: int,
+    schedule: Annotated[Schedule, AfterValidator(Schedule.model_validate)],
+    session: Annotated[Session, Depends(get_session)],
+) -> Schedule:
+    schedule.therapist = session.exec(
+        select(Therapist).where(Therapist.id == therapist_id)
+    ).one()
+    session.add(schedule)
+    session.commit()
+    session.refresh(schedule)
+    return schedule
+
+
+@router.get("/therapist/{therapist_id}/schedules")
+def get_therapist_schedules(
+    therapist_id: int, session: Annotated[Session, Depends(get_session)]
+) -> Sequence[Schedule]:
+    therapist = session.exec(
+        select(Therapist).where(Therapist.id == therapist_id)
+    ).one()
+    return therapist.schedules
+
+
+@router.get("/therapist/{therapist_id}/appointments")
+def get_therapist_appointments(
+    therapist_id: int, session: Annotated[Session, Depends(get_session)]
+) -> Sequence[Appointment]:
+    therapist = session.exec(
+        select(Therapist).where(Therapist.id == therapist_id)
+    ).one()
+    return therapist.appointments
