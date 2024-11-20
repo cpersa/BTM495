c9c5a35b31959a71069ca4c0e28eba4414df8662
charleain <183850672+charleain@users.noreply.github.com>
2024-11-20 04:25:43 -0500

    Update

--- src/renova/app/index.py
+++ src/renova/app/index.py
@@ -3,8 +3,9 @@ from dataclasses import dataclass
 from tkinter import W
 from typing import Annotated, cast
 
+import jwt
 from fastapi import Depends, FastAPI, Request
-from fastapi.security import HTTPAuthorizationCredentials, APIKeyCookie
+from fastapi.security import APIKeyCookie, HTTPAuthorizationCredentials
 from fastapi.templating import Jinja2Templates
 from jinja2 import Environment, PackageLoader
 from sqlmodel import Session, select
@@ -12,11 +13,11 @@ from sqlmodel import Session, select
 from renova.app import therapists
 from renova.db import get_session, init_db
 from renova.models.users import Client, Therapist, User
-import jwt
 
 templates = Jinja2Templates(env=Environment(loader=PackageLoader("renova.app")))
 
 token_cookie = APIKeyCookie(name="session")
+
 
 @dataclass
 class Credential[T: User]:
@@ -26,16 +27,20 @@ class Credential[T: User]:
     @property
     def is_client(self) -> bool:
         return self.session.get(Client, self.user.id) is not None
-    
+
     @property
     def is_therapist(self) -> bool:
         return self.session.get(Therapist, self.user.id) is not None
-    
 
-def authenticate_as(user_type: type[User]):
-    async def authenticate(session: Annotated[Session, Depends(get_session)], token: Annotated[str, Depends(token_cookie)]) -> Credential:
+
+def authenticate_as[T: User](user_type: type[T]):
+    async def authenticate(
+        session: Annotated[Session, Depends(get_session)],
+        token: Annotated[str, Depends(token_cookie)],
+    ) -> Credential[T]:
         user = user_type.model_validate(jwt.decode(token))
-        return Credential(session, user)
+        return Credential[T](session, user)
+
     return authenticate
 
 
